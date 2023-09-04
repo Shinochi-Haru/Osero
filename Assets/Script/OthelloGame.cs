1042de39a91603e104d578ecce6ff99f253f39be
Shinochi-Haru <haru060109011305@gmail.com>
2023-09-04 13:34:53 +0900

    [add] 敵のAI作成

--- Assets/Script/OthelloGame.cs
+++ Assets/Script/OthelloGame.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System;
 
 public class OthelloGame : MonoBehaviour
 {
@@ -11,6 +13,16 @@ public class OthelloGame : MonoBehaviour
 
     private GameObject[,] grid;
     private int[,] initialPieces;
+    private int currentPlayer = 1; // 1: 黒、2: 白
+
+    private System.Random random = new System.Random(); // ランダム数生成用のオブジェクト
+
+    private Vector2Int[] directions = {
+        new Vector2Int(0, 1), new Vector2Int(0, -1),
+        new Vector2Int(-1, 0), new Vector2Int(1, 0),
+        new Vector2Int(-1, 1), new Vector2Int(1, 1),
+        new Vector2Int(-1, -1), new Vector2Int(1, -1)
+    };
 
     private void Start()
     {
@@ -53,18 +65,72 @@ public class OthelloGame : MonoBehaviour
     {
         int opponentPieceType = pieceType == 1 ? 2 : 1;
 
-        Vector2Int[] directions = {
-            new Vector2Int(0, 1), new Vector2Int(0, -1),
-            new Vector2Int(-1, 0), new Vector2Int(1, 0),
-            new Vector2Int(-1, 1), new Vector2Int(1, 1),
-            new Vector2Int(-1, -1), new Vector2Int(1, -1)
-        };
-
         foreach (Vector2Int dir in directions)
         {
             int r = row + dir.y;
             int c = col + dir.x;
             bool validDirection = false;
+            bool foundOpponentPiece = false;
+
+            List<Vector2Int> piecesToFlip = new List<Vector2Int>(); // ひっくり返す対象の石を保持するリスト
+
+            while (r >= 0 && r < rows && c >= 0 && c < columns)
+            {
+                int cellPieceType = initialPieces[r, c];
+
+                if (cellPieceType == 0)
+                    break;
+
+                if (cellPieceType == opponentPieceType)
+                    foundOpponentPiece = true;
+
+                if (cellPieceType == pieceType)
+                {
+                    validDirection = true;
+                    break;
+                }
+
+                if (foundOpponentPiece)
+                {
+                    // 挟まれた石をリストに追加
+                    piecesToFlip.Add(new Vector2Int(r, c));
+                }
+
+                r += dir.y;
+                c += dir.x;
+            }
+
+            if (validDirection)
+            {
+                // 挟まれた石をひっくり返す
+                foreach (Vector2Int pieceToFlip in piecesToFlip)
+                {
+                    initialPieces[pieceToFlip.x, pieceToFlip.y] = pieceType;
+
+                    Vector3 piecePosition = grid[pieceToFlip.x, pieceToFlip.y].transform.position;
+                    Quaternion rotation = Quaternion.identity;
+                    GameObject newPiecePrefab = pieceType == 1 ? blackPiecePrefab : whitePiecePrefab;
+                    Instantiate(newPiecePrefab, piecePosition, rotation);
+                }
+            }
+        }
+    }
+
+
+    public void PlacePiece(int row, int col)
+    {
+        if (initialPieces[row, col] != 0)
+            return;
+
+        int pieceType = currentPlayer;
+        int opponentPieceType = 3 - pieceType; // 相手の石の種類
+
+        bool isValidMove = false;
+
+        foreach (Vector2Int dir in directions)
+        {
+            int r = row + dir.y;
+            int c = col + dir.x;
             bool foundOpponentPiece = false;
 
             while (r >= 0 && r < rows && c >= 0 && c < columns)
@@ -79,7 +145,7 @@ public class OthelloGame : MonoBehaviour
 
                 if (cellPieceType == pieceType && foundOpponentPiece)
                 {
-                    validDirection = true;
+                    isValidMove = true;
                     break;
                 }
 
@@ -87,50 +153,35 @@ public class OthelloGame : MonoBehaviour
                 c += dir.x;
             }
 
-            if (validDirection)
+            if (isValidMove)
             {
-                r = row + dir.y;
-                c = col + dir.x;
-
-                while (initialPieces[r, c] == opponentPieceType)
-                {
-                    initialPieces[r, c] = pieceType;
-
-                    Vector3 piecePosition = grid[r, c].transform.position;
-                    Quaternion rotation = Quaternion.identity;
-                    GameObject newPiecePrefab = pieceType == 1 ? blackPiecePrefab : whitePiecePrefab;
-                    Instantiate(newPiecePrefab, piecePosition, rotation);
-
-                    r += dir.y;
-                    c += dir.x;
-                }
+                break;
             }
         }
-    }
 
-    public void PlacePiece(int row, int col)
-    {
-        if (initialPieces[row, col] != 0)
-            return;
+        if (isValidMove)
+        {
+            initialPieces[row, col] = pieceType;
+            PlacePiece(row, col, pieceType);
+            FlipPieces(row, col, pieceType);
 
-        int pieceType = 1;
-        initialPieces[row, col] = pieceType;
-        FlipPieces(row, col, pieceType);
-        PlacePiece(row, col, pieceType); // ここでPlacePieceを呼び出して石を表示
+            // ターンを交代
+            currentPlayer = 3 - currentPlayer;
+        }
     }
 
     void SetupInitialPieces()
     {
         initialPieces = new int[,]
         {
-            {0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 1, 2, 0, 0, 0},
-            {0, 0, 0, 2, 1, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0}
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 1, 2, 0, 0, 0},
+        {0, 0, 0, 2, 1, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0}
         };
 
         for (int row = 0; row < rows; row++)
@@ -142,4 +193,96 @@ public class OthelloGame : MonoBehaviour
             }
         }
     }
+
+
+    private List<Vector2Int> FindLegalMoves(int player)
+    {
+        List<Vector2Int> legalMoves = new List<Vector2Int>();
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < columns; col++)
+            {
+                if (initialPieces[row, col] == 0)
+                {
+                    if (IsMoveLegal(row, col, player))
+                    {
+                        legalMoves.Add(new Vector2Int(row, col));
+                    }
+                }
+            }
+        }
+
+        return legalMoves;
+    }
+
+    private bool IsMoveLegal(int row, int col, int player)
+    {
+        int opponentPieceType = 3 - player;
+
+        foreach (Vector2Int dir in directions)
+        {
+            int r = row + dir.y;
+            int c = col + dir.x;
+            bool foundOpponentPiece = false;
+
+            while (r >= 0 && r < rows && c >= 0 && c < columns)
+            {
+                int cellPieceType = initialPieces[r, c];
+
+                if (cellPieceType == 0)
+                    break;
+
+                if (cellPieceType == opponentPieceType)
+                    foundOpponentPiece = true;
+
+                if (cellPieceType == player && foundOpponentPiece)
+                {
+                    return true;
+                }
+
+                r += dir.y;
+                c += dir.x;
+            }
+        }
+
+        return false;
+    }
+
+    private void Update()
+    {
+        // 現在のプレイヤーがAIの場合、合法な手を選ぶ
+        if (currentPlayer == 2) // AIは2番目のプレイヤーと仮定
+        {
+            List<Vector2Int> legalMoves = FindLegalMoves(currentPlayer);
+            if (legalMoves.Count > 0)
+            {
+                // AIが合法な手を選ぶ
+                Vector2Int bestMove = (Vector2Int)ChooseBestMove(legalMoves, currentPlayer);
+
+                // 合法な手を選んだ場合にのみ石を置く
+                if (bestMove != null)
+                {
+                    PlacePiece(bestMove.x, bestMove.y);
+                    FlipPieces(bestMove.x, bestMove.y, currentPlayer); // 石をひっくり返す
+                }
+            }
+        }
+    }
+
+    // 合法な手の中からAIが最適な手を選ぶロジックを追加
+    private Vector2Int? ChooseBestMove(List<Vector2Int> legalMoves, int player)
+    {
+        // ここに最適な手の選択ロジックを実装します。
+        // この例ではランダムな手を選びますが、AIの戦略に応じて変更してください。
+
+        if (legalMoves.Count > 0)
+        {
+            int randomIndex = random.Next(0, legalMoves.Count);
+            return legalMoves[randomIndex];
+        }
+
+        return null; // 合法な手がない場合
+    }
+
 }
